4b870806f2eecd39b81342ea0948e5250a4d88c3
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-05-31 14:29:39 +0200

    pushing clean tutorial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 };
-  const link = "https://google.com";
-
-
   return (
     <div className="App">
       <div className="content">
