a9c16d602917893a92974f62cb280a6a460e64e1
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-06-03 19:14:48 +0200

    started function tutorial

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,14 @@
 
 export function Home() {
+
+    function handleClick() {
+        console.log('hello ninjas')
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
         </div>
     )
 }
