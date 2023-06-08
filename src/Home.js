a7e9cbdf20ccccfde65bbdc0ca3c28e4f27257fb
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-06-08 15:28:23 +0200

    event handler lesson nr 7 done

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,15 @@ export function Home() {
     function handleClick() {
         console.log('hello ninjas')
     }
+    function handleClickAgain(name) {
+        console.log('hello ' + name)
+    }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('mario')}>Click me again</button>
         </div>
     )
 }
