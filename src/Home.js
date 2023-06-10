f4d601c98f9de7470d2510cfb4fa90c4ea14b39f
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-06-10 19:17:57 +0200

    Props lesson done

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BlogList } from './BlogList';
 
 export function Home() {
 
@@ -11,12 +12,8 @@ export function Home() {
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2> {blog.title}</h2>
-                    <p>Written by: {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All blogs!" />
         </div>
     )
 }
+
